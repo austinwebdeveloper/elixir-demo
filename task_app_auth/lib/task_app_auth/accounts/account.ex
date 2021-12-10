8b701a1f2b1c755b4dfc38e1a1a259a447714ab2
Ravi Kallepalli <ravi@austinwebdeveloper.co>
2021-12-10 09:22:10 +0000

    Added code for file upload

--- task_app_auth/lib/task_app_auth/accounts/account.ex
+++ task_app_auth/lib/task_app_auth/accounts/account.ex
@@ -3,19 +3,18 @@ defmodule TaskAppAuth.Accounts.Account do
   import Ecto.Changeset
 
   schema "account" do
-    field :city, :string
-    field :dob, :string
-    field :email, :string
-    field :first_name, :string
-    field :last_name, :string
-    field :password, :string
-    field :phone_number, :string
-    field :ssn, :string
-    field :state, :string
-    field :street_address, :string
-    field :zip_code, :integer
-    field :image_upload, :string
-
+    field(:city, :string)
+    field(:dob, :string)
+    field(:email, :string)
+    field(:first_name, :string)
+    field(:last_name, :string)
+    field(:password, :string)
+    field(:phone_number, :string)
+    field(:ssn, :string)
+    field(:state, :string)
+    field(:street_address, :string)
+    field(:zip_code, :integer)
+    field(:image_upload, :string)
 
     timestamps()
   end
@@ -23,7 +22,32 @@ defmodule TaskAppAuth.Accounts.Account do
   @doc false
   def changeset(account, attrs) do
     account
-    |> cast(attrs, [:first_name, :last_name, :email, :dob, :phone_number, :street_address, :zip_code, :city, :state, :password, :ssn, :image_upload])
-    |> validate_required([:first_name, :last_name, :email, :dob, :phone_number, :street_address, :zip_code, :city, :state, :password, :ssn])
+    |> cast(attrs, [
+      :first_name,
+      :last_name,
+      :email,
+      :dob,
+      :phone_number,
+      :street_address,
+      :zip_code,
+      :city,
+      :state,
+      :password,
+      :ssn,
+      :image_upload
+    ])
+    |> validate_required([
+      :first_name,
+      :last_name,
+      :email,
+      :dob,
+      :phone_number,
+      :street_address,
+      :zip_code,
+      :city,
+      :state,
+      :password,
+      :ssn
+    ])
   end
 end
--- task_app_auth/lib/task_app_auth_web/live/account_live/new.ex
+++ task_app_auth/lib/task_app_auth_web/live/account_live/new.ex
@@ -12,19 +12,19 @@ defmodule TaskAppAuthWeb.AccountLive.New do
     display = 0
     # {:ok, assign(socket, changeset: changeset, display: 0)}
 
-    {:ok, socket
-    |> assign(:changeset, changeset)
-    |> assign(:display,display)
-    |> allow_upload(:image, accept: ~w(.jpg .jpeg .png), max_entries: 1)}
-
-
+    {:ok,
+     socket
+     #  |> assign(socket.assigns)
+     |> assign(:changeset, changeset)
+     |> assign(:display, display)
+     |> allow_upload(:image, accept: ~w(.jpg .jpeg .png), max_entries: 3)}
   end
 
   def handle_event("validate", %{"account" => user_params}, socket) do
     # IO.inspect(socket.assigns.changeset.changes)
-    Logger.info("before user params")
-    IO.inspect(user_params)
-    Logger.info("after user params")
+    # Logger.info("before user params")
+    # IO.inspect(socket.assigns.uploads)
+    # Logger.info("after user params")
 
     # IO.inspect(socket.assigns.changeset.changes)
 
@@ -57,21 +57,29 @@ defmodule TaskAppAuthWeb.AccountLive.New do
   end
 
   def handle_event("save", %{"account" => user_params}, socket) do
-    Logger.info(" submitted from ")
+    # Logger.info(" submitted from ")
+    # IO.inspect(socket.assigns)
+
     file_path =
       consume_uploaded_entry(socket, :image, fn %{path: path}, _entry ->
-        dest = Path.join("priv/static/uploads", Path.basename(path))
-        File.cp!(path, dest)
-        Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
+        # dest = Path.join("priv/static/uploads", Path.basename(path))
+        # File.cp!(path, dest)
+        IO.inspect("inspect")
+        # Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
       end)
-    case Accounts.create_account(Map.put(user_params, :image_upload, file_path)) do
+
+    IO.inspect(file_path)
+
+    case Accounts.create_account(Map.put(user_params, "image_upload", file_path)) do
+      # case Accounts.create_account(user_params) do
       {:ok, account} ->
         {:noreply,
          socket
          |> put_flash(:info, "user created")}
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, changeset: changeset)}
+        IO.inspect("error")
+        # {:noreply, assign(socket, changeset: changeset)}
     end
   end
 end
