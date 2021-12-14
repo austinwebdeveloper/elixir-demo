fd32c0a09fd0c866abef6a6eb1e5565e588192d6
Ravi Kallepalli <ravi@austinwebdeveloper.co>
2021-12-14 12:04:33 +0000

    Added disabled prop for button

--- task_app_auth/lib/task_app_auth_web/live/account_live/new.ex
+++ task_app_auth/lib/task_app_auth_web/live/account_live/new.ex
@@ -11,6 +11,7 @@ defmodule TaskAppAuthWeb.AccountLive.New do
     changeset = Accounts.change_account(%Account{})
     display = 0
     success = true
+    disable = true
     # {:ok, assign(socket, changeset: changeset, display: 0)}
     {:ok,
      socket
@@ -18,58 +19,72 @@ defmodule TaskAppAuthWeb.AccountLive.New do
      |> assign(:changeset, changeset)
      |> assign(:display, display)
      |> assign(:success, success)
+     |> assign(:disable, disable)
      |> allow_upload(:image, accept: ~w(.jpg .jpeg .png .pdf), max_entries: 1)}
   end
 
   def handle_event("validate", %{"account" => user_params}, socket) do
-    IO.inspect(user_params)
-    # Logger.info("before user params")
-    # IO.inspect(socket.assigns.uploads)
-    # Logger.info("after user params")
+    display = socket.assigns.display
 
-    # IO.inspect(socket.assigns.changeset.changes)
+    btn =
+      case display do
+        0 ->
+          if user_params["first_name"] != "" and user_params["last_name"] != "" and
+               user_params["email"] != "" do
+            false
+          end
+
+        1 ->
+          if user_params["dob"] != "" and user_params["phone_number"] != "" do
+            false
+          end
+
+        2 ->
+          if user_params["street_address"] != "" and user_params["zip_code"] != "" and
+               user_params["city"] != "" and user_params["state"] != "" do
+            false
+          end
+
+        3 ->
+          if user_params["password"] != "" and user_params["ssn"] != "" do
+            false
+          end
+
+        _ ->
+          false
+      end
 
     changeset =
       %Account{}
       |> TaskAppAuth.Accounts.change_account(user_params)
       |> Map.put(:action, :insert)
 
-    {:noreply, assign(socket, changeset: changeset)}
+    {:noreply, assign(socket, changeset: changeset, disable: "#{btn}")}
   end
 
   def handle_event("backward", _, socket) do
-    # Logger.info("clicked backward")
-    # IO.inspect(socket)
-
     count = socket.assigns.display - 1
+    # socket = assign(socket, display: count, disable: "false")
     socket = assign(socket, :display, count)
     {:noreply, socket}
   end
 
   def handle_event("forward", _, socket) do
     # Logger.info("clicked forward")
-    # IO.inspect(socket.assigns.changeset.changes)
-
     count = socket.assigns.display + 1
-
+    # socket = assign(socket, display: count, disable: "true")
     socket = assign(socket, :display, count)
-
     {:noreply, socket}
   end
 
   def handle_event("save", %{"account" => user_params}, socket) do
-    # Logger.info(" submitted from ")
-    # IO.inspect(socket.assigns)
-
     file_path =
       consume_uploaded_entries(socket, :image, fn %{path: path}, _entry ->
         dest = Path.join("priv/static/uploads", Path.basename(path))
-        IO.inspect(path)
         File.cp!(path, dest)
         Routes.static_path(socket, "/uploads/#{Path.basename(dest)}")
       end)
 
-    IO.inspect(file_path)
     [head | tail] = file_path
 
     case Accounts.create_account(Map.put(user_params, "image_upload", head)) do
@@ -81,7 +96,6 @@ defmodule TaskAppAuthWeb.AccountLive.New do
          |> put_flash(:info, "Account created")}
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        IO.inspect(changeset)
         {:noreply, assign(socket, changeset: changeset)}
     end
   end
